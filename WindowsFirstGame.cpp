cf15a8e83d61850f0114b323562f8a4a7c326a41
240026 <240026@jc-21.jp>
2025-06-27 14:12:28 +0900

    緑色のウィンドウが出るとこまで 右上の×は反応しないものとする

--- WindowsFirstGame.cpp
+++ WindowsFirstGame.cpp
@@ -30,8 +30,23 @@
 
 #include "framework.h"
 #include "WindowsFirstGame.h"
+#include <d3d11.h>
+
+//リンカ
+#pragma comment(lib,"d3d11.lib")
+
+HWND hWnd = nullptr;
+
+ID3D11Device* pDevice;		//デバイス
+ID3D11DeviceContext* pContext;		//デバイスコンテキスト
+IDXGISwapChain* pSwapChain;		//スワップチェイン
+ID3D11RenderTargetView* pRenderTargetView;	//レンダーターゲットビュー
 
 #define MAX_LOADSTRING 100
+
+const wchar_t* WIN_CLASS_NAME = L"SAMPLE GAME WINDOW";
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
 
 // グローバル変数:
 HINSTANCE hInst;                                // 現在のインターフェイス
@@ -67,16 +82,90 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // TODO: ここにコードを挿入してください。
 
+    //szWindowClass = WIN_CLASS_NAME;
+
     // グローバル文字列を初期化する
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_WINDOWSFIRSTGAME, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
+
+    //ウィンドウサイズの計算
+    RECT winRect = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+    AdjustWindowRect(&winRect, WS_OVERLAPPEDWINDOW, FALSE);
+    int winW = winRect.right - winRect.left;     //ウィンドウ幅
+    int winH = winRect.bottom - winRect.top;     //ウィンドウ高さ
 
     // アプリケーション初期化の実行:
     if (!InitInstance (hInstance, nCmdShow))
     {
         return FALSE;
     }
+
+    //Direct3Dの初期化
+    DXGI_SWAP_CHAIN_DESC scDesc = {};
+
+    //とりあえず全部0
+    ZeroMemory(&scDesc, sizeof(scDesc));
+
+    //描画先のフォーマット
+    scDesc.BufferDesc.Width = WINDOW_WIDTH;		//画面幅
+    scDesc.BufferDesc.Height = WINDOW_HEIGHT;	//画面高さ
+    scDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;	// 何色使えるか
+
+    //FPS（1/60秒に1回）
+    scDesc.BufferDesc.RefreshRate.Numerator = 60;
+    scDesc.BufferDesc.RefreshRate.Denominator = 1;
+
+    //その他
+    scDesc.Windowed = TRUE;			//ウィンドウモードかフルスクリーンか
+    scDesc.OutputWindow = hWnd;		//ウィンドウハンドル
+    scDesc.BufferCount = 1;			//バックバッファの枚数
+    scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;	//バックバッファの使い道＝画面に描画するために
+    scDesc.SampleDesc.Count = 1;		//MSAA（アンチエイリアス）の設定
+    scDesc.SampleDesc.Quality = 0;		//　〃
+
+    ////////////////上記設定をもとにデバイス、コンテキスト、スワップチェインを作成////////////////////////
+    D3D_FEATURE_LEVEL level;
+    D3D11CreateDeviceAndSwapChain(
+        nullptr,				// どのビデオアダプタを使用するか？既定ならばnullptrで
+        D3D_DRIVER_TYPE_HARDWARE,		// ドライバのタイプを渡す。ふつうはHARDWARE
+        nullptr,				// 上記をD3D_DRIVER_TYPE_SOFTWAREに設定しないかぎりnullptr
+        0,					// 何らかのフラグを指定する。（デバッグ時はD3D11_CREATE_DEVICE_DEBUG？）
+        nullptr,				// デバイス、コンテキストのレベルを設定。nullptrにしとけばOK
+        0,					// 上の引数でレベルを何個指定したか
+        D3D11_SDK_VERSION,			// SDKのバージョン。必ずこの値
+        &scDesc,				// 上でいろいろ設定した構造体
+        &pSwapChain,				// 無事完成したSwapChainのアドレスが返ってくる
+        &pDevice,				// 無事完成したDeviceアドレスが返ってくる
+        &level,					// 無事完成したDevice、Contextのレベルが返ってくる
+        &pContext);				// 無事完成したContextのアドレスが返ってくる
+
+    ///////////////////////////レンダーターゲットビュー作成///////////////////////////////
+//スワップチェーンからバックバッファを取得（バックバッファ ＝ レンダーターゲット）
+    ID3D11Texture2D* pBackBuffer;
+    pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+    //レンダーターゲットビューを作成
+    pDevice->CreateRenderTargetView(pBackBuffer, NULL, &pRenderTargetView);
+
+    //一時的にバックバッファを取得しただけなので解放
+    pBackBuffer->Release();
+
+    ///////////////////////////ビューポート（描画範囲）設定///////////////////////////////
+//レンダリング結果を表示する範囲
+    D3D11_VIEWPORT vp;
+    vp.Width = (float)WINDOW_WIDTH;	//幅
+    vp.Height = (float)WINDOW_HEIGHT;//高さ
+    vp.MinDepth = 0.0f;	//手前
+    vp.MaxDepth = 1.0f;	//奥
+    vp.TopLeftX = 0;	//左
+    vp.TopLeftY = 0;	//上
+
+    //データを画面に描画するための一通りの設定（パイプライン）
+    pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);  // データの入力種類を指定
+    pContext->OMSetRenderTargets(1, &pRenderTargetView, nullptr);            // 描画先を設定
+    pContext->RSSetViewports(1, &vp);
+
 
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSFIRSTGAME));
 
@@ -85,10 +174,25 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // メイン メッセージ ループ:
     while (GetMessage(&msg, nullptr, 0, 0))
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        //メッセージあり
+        if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+
         {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
+
+        }
+        //メッセージなし
+        else
+        {
+            //ゲームの処理
+            //背景の色
+            float clearColor[4] = { 0.0f, 0.5f, 0.5f, 1.0f };//R,G,B,A
+            //画面をクリア
+            pContext->ClearRenderTargetView(pRenderTargetView, clearColor);
+            //描画処理
+            //スワップ（バックバッファを表に表示する）
+            pSwapChain->Present(0, 0);
         }
     }
 
@@ -192,7 +296,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // グローバル変数にインスタンス ハンドルを格納する
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+   hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
